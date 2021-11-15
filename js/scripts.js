b3a1e567dc1cae7332f6fef2f926bcfa86158014
dhedez <93016227+DHedezBadillo@users.noreply.github.com>
2021-11-14 17:08:30 -0800

    Added an array of Pokemon objects
    The array lists Pokemon objects and their key characteristics

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-alert('Hello World');
-var favoriteFood = 'Chicken';
-document.write(favoriteFood);
+let pokemonList = [
+
+    {
+        name: 'Golem',
+        height: 1.4,
+        type: ['rock', 'ground']
+    },
+
+    {
+        name: 'Venusaur',
+        height: 1.4,
+        type: ['grass', 'poison']
+    },
+
+    {
+        name: 'Pidgey',
+        height: 0.3,
+        type: ['flying', 'normal']
+    },
+
+    {
+        name: ' Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+
+    {
+        name: 'Weedle',
+        height: 0.3,
+        type: ['bug', 'poison']
+    }
+
+];
