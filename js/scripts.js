6cd037baf64de85f4cad6c9c127321632bd59108
dhedez <93016227+DHedezBadillo@users.noreply.github.com>
2021-12-23 13:29:14 -0800

    Update scripts.js
    wrapped pokemonList within an IIFE
    created a forEach loop to iterate over pokemonList items and print them

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,74 @@
-const pokemonList = [
+//Creates a pokemon repository within an IIFE 
+let pokemonRepository = (function () {
+    const pokemonList = [
 
-    {
-        name: 'Golem',
-        height: 1.4,
-        type: ['rock', 'ground']
-    },
+        {
+            name: 'Golem',
+            height: 1.4,
+            type: ['rock', 'ground']
+        },
+    
+        {
+            name: 'Venusaur',
+            height: 1.4,
+            type: ['grass', 'poison']
+        },
+    
+        {
+            name: 'Pidgey',
+            height: 0.3,
+            type: ['flying', 'normal']
+        },
+    
+        {
+            name: ' Charizard',
+            height: 1.7,
+            type: ['fire', 'flying']
+        },
+    
+        {
+            name: 'Weedle',
+            height: 0.3,
+            type: ['bug', 'poison']
+        }
+    
+    ];
 
-    {
-        name: 'Venusaur',
-        height: 1.4,
-        type: ['grass', 'poison']
-    },
-
-    {
-        name: 'Pidgey',
-        height: 0.3,
-        type: ['flying', 'normal']
-    },
-
-    {
-        name: ' Charizard',
-        height: 1.7,
-        type: ['fire', 'flying']
-    },
-
-    {
-        name: 'Weedle',
-        height: 0.3,
-        type: ['bug', 'poison']
+    //A function to add new Pokemons to the repository, checks to make sure it is an object, and returns a message when criteria is not met.
+    function add(addedPokemon) {
+        if (typeof addedPokemon === 'object') {
+        pokemonList.push(addedPokemon);
+        } else {
+            console.log('Cannot add - this must not be an object');
+        }
     }
+      
+    //Function to return all item within the pokemonList array.
+    function getAll() {
+        return pokemonList;
+    }
+    //Returns the functions declared above.    
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
-];
+//Adds new pokemon and its height
+pokemonRepository.add({name: 'Pikachu', height: 1.3})
 
-//Lists Pokemon names and heights
-for (let i=0; i<pokemonList.length; i++) {
-    document.write ('<div>'+pokemonList [i].name + ' (height: ' + pokemonList [i].height + '), </div>');
-
-//checks for a height of 1.7 or higher, then gives a message if true   
-    if (pokemonList [i].height >=1.7) {
-    document.write ('Wow, that\'s big!');
+//A function to print the names and heights of all pokemons, and also to return a message if it is bigger than 1.7
+function printDetails(list) {
+    list.forEach(function(pokemon) {
+        if (pokemon.height >= 1.7){
+            document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow - that\'s big! </p>');
+        } else {
+            document.write('<p>' + pokemon.name + ' (height: ' +pokemon.height + ') </p>');
+        }
+    });
 }
-}
+
+//Calls the printDetails function to print out the pokemonRepository.
+printDetails(pokemonRepository.getAll());
+
+
